458383006e3b9eed266cb671b8c0e35a0677857f
Francisco José García Rico <fgarciarico@gmail.com>
2015-10-19 20:08:23 +0100

    Deleting even tags

--- grails-app/assets/javascripts/reactjs/views/src/tag/ListTag.js
+++ grails-app/assets/javascripts/reactjs/views/src/tag/ListTag.js
@@ -21,14 +21,37 @@ var ListTag = React.createClass({
     handleEditTagClick: function(tag) {
         this.setState({tag:tag});
     },
+    handleDeleteTagClick: function(tag) {
+        var _this = this;
+        tag.destroy({
+            success: function(model, response) {
+                console.log("Tag "+tag+" deleted successfully");
+            },
+            error: function(model, response) {
+                console.log("ERROR deleting tag "+tag);
+                _this.props.collection.fetch();
+            }
+        });
+    },
     render: function() {
         var _this = this;
         var tags = this.props.collection.models.map(function(tag) {
             var editTagHandler = function() {
                 _this.handleEditTagClick(tag);
             }
+            var deleteTagHandler = function() {
+                _this.handleDeleteTagClick(tag);
+            }
+            var actionButtons = function() {
+                return (
+                    <span>
+                        <ReactBootstrap.Button type="button" bsStyle="primary" onClick={editTagHandler}>Edit</ReactBootstrap.Button>&nbsp;
+                        <ReactBootstrap.Button type="button" bsStyle="warning" onClick={deleteTagHandler}>Delete</ReactBootstrap.Button>
+                    </span>    
+                )
+            }
             return (
-                <TableRow key={tag.get("id")} data={[tag.get("id"), tag.get("name"), '']} onClick={editTagHandler}/>
+                <TableRow key={tag.get("id")} data={[tag.get("id"), tag.get("name"), actionButtons()]}/>
             );
         });
         return (
